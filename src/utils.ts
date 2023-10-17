47b2f7cd49a65f4194ec67688c594e493d2a9783
Yugang Cao <1442770751@qq.com>
2023-10-17 22:21:58 +0800

    fix: use pangu to add space (#20)

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,15 @@
 import type { TextEditorEdit } from 'vscode'
 import { Position, Range, window as Window, workspace as Workspace } from 'vscode'
+import pangu from 'pangu'
 
 export function addSpaceChineseAndEnglish(line: string) {
   const regexCE = /([\u4E00-\u9FA5]+)([a-zA-Z0-9]+)/
   const regexEC = /([a-zA-Z0-9]+)([\u4E00-\u9FA5]+)/
   let changed = false
-  let matched = line.match(regexCE) || line.match(regexEC)
-  while (matched) {
-    line = line.replace(regexCE, (_, chinese, english) => `${chinese} ${english}`)
-    line = line.replace(regexEC, (_, english, chinese) => `${english} ${chinese}`)
-    matched = line.match(regexCE) || line.match(regexEC)
+  do {
+    line = pangu.spacing(line)
     changed = true
-  }
+  } while (line.match(regexCE) || line.match(regexEC))
   return {
     line,
     changed,
@@ -28,14 +26,32 @@ export function getAutoSapceConfig() {
   }
 }
 
-export function autoAddSpaceAll(text: string) {
+export function autoAddSpace(text: string) {
   const editor = Window.activeTextEditor
   if (!editor)
     return
   const document = editor.document
-  const lines = text.split(/\n/g)
+  const { enable, comment } = getAutoSapceConfig()
+  if (!enable)
+    return
+  let lines: string[] = []
+  if (comment === 'comment') {
+    // javascript java c++ c# php swift
+    const commentRegex = /\/\/.*|\/\*[\s\S]*?\*\//gm
+    // python
+    const pythonRegex = /(\"\"\"|\'\'\')([\s\S]*?)(\"\"\"|\'\'\')|(#.*$)/gm
+    // ruby
+    const rubyRegex = /#\s*(.*)$/gm
+    const commonlines = text.match(commentRegex)
+    const pythonlines = text.match(pythonRegex)
+    const rubylines = text.match(rubyRegex)
+    lines = commonlines?.concat(pythonlines ?? []).concat(rubylines ?? []) as string[]
+  }
+  else {
+    lines = text.split(/\n/g)
+  }
   const updatedLines = lines?.map((line) => {
-    return addSpaceChineseAndEnglish(line).line
+    return pangu.spacing(line)
   })
   const updatedText = updatedLines.join('\n')
   const start = new Position(0, 0)
@@ -44,36 +60,4 @@ export function autoAddSpaceAll(text: string) {
   editor.edit((editBuilder: TextEditorEdit) => {
     editBuilder.replace(range, updatedText)
   })
-}
-
-export async function autoAddSpaceComment(text: string) {
-  const editor = Window.activeTextEditor
-  if (!editor)
-    return
-  const document = editor.document
-  // javascript java c++ c# php swift
-  const commentRegex = /\/\/.*|\/\*[\s\S]*?\*\//gm
-  // python
-  const pythonRegex = /(\"\"\"|\'\'\')([\s\S]*?)(\"\"\"|\'\'\')|(#.*$)/gm
-  // ruby
-  const rubyRegex = /#\s*(.*)$/gm
-  const commonlines = text.match(commentRegex)
-  const pythonlines = text.match(pythonRegex)
-  const rubylines = text.match(rubyRegex)
-  const lines = commonlines?.concat(pythonlines ?? []).concat(rubylines ?? [])
-  if (!lines || lines?.length === 0)
-    return
-  for (let i = 0; i < lines?.length; i++) {
-    const line = lines[i]
-    const { line: replaceLine, changed } = addSpaceChineseAndEnglish(line)
-    if (!changed)
-      continue
-    const offset = document.getText().indexOf(line)
-    const start = document.positionAt(offset)
-    const end = document.positionAt(offset + line.length)
-    const range = new Range(start, end)
-    await editor.edit((editBuilder: TextEditorEdit) => {
-      editBuilder.replace(range, replaceLine)
-    })
-  }
 }
