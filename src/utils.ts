82715c5461efe7157d7b937cceda61f9933e54af
Yugang Cao <1442770751@qq.com>
2023-10-17 21:06:23 +0800

    fix: optimize regexp

--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,15 @@ import type { TextEditorEdit } from 'vscode'
 import { Position, Range, window as Window, workspace as Workspace } from 'vscode'
 
 export function addSpaceChineseAndEnglish(line: string) {
-  const regexCE = /\s*?.*?([\u4E00-\u9FA5]+)([a-zA-Z0-9]+).*/
-  const regexEC = /\s*?.*?([a-zA-Z0-9]+)([\u4E00-\u9FA5]+).*/
-  let match = regexCE.exec(line) || regexEC.exec(line)
+  const regexCE = /([\u4E00-\u9FA5]+)([a-zA-Z0-9]+)/
+  const regexEC = /([a-zA-Z0-9]+)([\u4E00-\u9FA5]+)/
   let changed = false
-  while (match !== null) {
-    const chinese = match[1]
-    const english = match[2]
-    line = line.replace(chinese + english, `${chinese} ${english}`)
+  let matched = line.match(regexCE) || line.match(regexEC)
+  while (matched) {
+    line = line.replace(regexCE, (_, chinese, english) => `${chinese} ${english}`)
+    line = line.replace(regexEC, (_, english, chinese) => `${english} ${chinese}`)
+    matched = line.match(regexCE) || line.match(regexEC)
     changed = true
-    match = regexCE.exec(line) || regexEC.exec(line)
   }
   return {
     line,
